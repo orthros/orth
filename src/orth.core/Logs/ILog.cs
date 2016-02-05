2ce26469d34885a8e95a4d61b5b9cc4beb4f0cda
orthros <colinnlsn@gmail.com>
2016-02-05 06:49:22 -0600

    Initial code commit. Added a logging utility and a Utility class (creates random strings)

---
+++ src/orth.core/Logs/ILog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Orth.Core.Logs
+{
+    /// <summary>
+    /// Interface for Logging
+    /// </summary>
+    public interface ILog
+    {
+        /// <summary>
+        /// Logs the passed string. 
+        /// </summary>
+        /// <param name="message">The Message to Log</param>
+        void Log(string message);
+    }
+}
