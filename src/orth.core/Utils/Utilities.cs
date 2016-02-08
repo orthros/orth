938e04490dbeccde7d62c809d0feff5b22766d19
orthros <colinnlsn@gmail.com>
2016-02-08 08:30:06 -0600

    Added a summary to the RandomString function. Added a new function ParseTrueFalse which parses a string to a bool and takes a default value Note, this needs to be Unit-tested.

--- src/orth.core/Utils/Utilities.cs
+++ src/orth.core/Utils/Utilities.cs
@@ -9,7 +9,12 @@ namespace Orth.Core.Utils
     public static class Utilities
     {
         private static Random random = new Random((int)DateTime.Now.Ticks);
-
+        
+        /// <summary>
+        /// Generates a random string of given <paramref name="size"/>
+        /// </summary>
+        /// <param name="size">The length of the string to generate</param>
+        /// <returns></returns>
         public static string RandomString(int size)
         {
             StringBuilder builder = new StringBuilder();
@@ -21,6 +26,41 @@ namespace Orth.Core.Utils
             }
 
             return builder.ToString();
+        }
+
+        /// <summary>
+        /// Parses a string into true or false. If the string cannot be parsed, it returns the default value
+        /// </summary>
+        /// <param name="value">The string to parse</param>
+        /// <param name="defaultValue">The default value to use if the string was not able to be parsed</param>
+        /// <returns></returns>
+        public static bool ParseTrueFalse(string value, bool defaultValue = false)
+        {
+            bool retVal = defaultValue;
+
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                switch (value.ToLowerInvariant())
+                {
+                    case "yes":
+                    case "y":
+                    case "true":
+                    case "t":
+                    case "on":
+                    case "1":
+                        retVal = true;
+                        break;
+                    case "no":
+                    case "n":
+                    case "false":
+                    case "f":
+                    case "off":
+                    case "0":
+                        retVal = false;
+                        break;
+                }
+            }
+            return retVal;
         }
     }
 }
