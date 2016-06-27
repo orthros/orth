6248edc75824f093e8a84066abbbaeb36b43752c
Colin <colinnlsn@gmail.com>
2016-06-26 19:19:44 -0500

    index on fb_dnxprojects: 933c618 Added nuspec file for orth.core

---
+++ src/orth.tests/EnumTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using orth.core.Utils;
+using orth.tests.Stubs;
+using Xunit;
+
+namespace orth.tests
+{
+    public class EnumTests
+    {
+        [Fact]
+        void TestTriState()
+        {
+            var expected = new HashSet<TriStateEnum>() { TriStateEnum.Item1, TriStateEnum.Item2, TriStateEnum.Item3 };
+            var vals = EnumUtils.GetValues<TriStateEnum>();
+            Assert.Equal(vals.Count(), expected.Count);
+            
+            var valsSet = new HashSet<TriStateEnum>(vals);
+            Assert.Subset(expected, valsSet);
+            Assert.Subset(valsSet, expected);
+        }
+    }
+}
---
+++ src/orth.tests/Stubs/TriStateEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orth.tests.Stubs
+{
+    public enum TriStateEnum
+    {
+        Item1,
+        Item2,
+        Item3
+    }
+}
